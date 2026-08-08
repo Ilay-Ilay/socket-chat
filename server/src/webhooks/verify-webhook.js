41291be33564a64fd46ca07a5558a407a7157937
Ilay Zeitlin <zeitlinilya@gmail.com>
2026-08-08 18:40:49 +0300

    webhook comment

--- server/src/webhooks/verify-webhook.js
+++ server/src/webhooks/verify-webhook.js
@@ -1,21 +1,25 @@
 import { verifyWebhook } from "@clerk/express/webhooks";
 
 export default async function webhook(req, res) {
-  try {
-    const event = await verifyWebhook(req);
-    console.log(
-      "******************************************************************",
-    );
-    console.log(event);
-    const eventType = evt.type;
-    console.log(eventType);
+  console.log(
+    "******************************************************************",
+  );
+  console.log("WEBHOOK RECIEVED");
 
-    console.log(req.data);
-    console.log(
-      "******************************************************************",
-    );
-    res.sendStatus(200);
-  } catch (error) {
-    console.error("Webhook verification failed:", error);
-  }
+  //   try {
+  //     const event = await verifyWebhook(req);
+
+  //     console.log(event);
+  //     const eventType = event.type;
+  //     console.log(eventType);
+
+  //     console.log(req.data);
+  //     console.log(
+  //       "******************************************************************",
+  //     );
+  //     res.sendStatus(200);
+  //   } catch (error) {
+  //     console.error("Webhook verification failed:", error);
+  //     res.status(400).json({ message: "Webhook verification failed", error });
+  //   }
 }
