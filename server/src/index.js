28e6cc00b6761a2ab16c90c02769ddfe1fce1007
Ilay Zeitlin <zeitlinilya@gmail.com>
2026-08-08 15:46:00 +0300

    cors middleware added with origin

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const APP_URL = process.env.APP_URL;
+
+app.use(express.json());
+
+app.use(cors({ origin: APP_URL }));
 
 app.listen(PORT, () => {
   console.log("APP IS NOW RUNNING ON PORT: ", PORT);
