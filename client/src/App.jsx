3e6ab3f8f93020b5806f8abd44e21dcfec432f03
Ilay Zeitlin <zeitlinilya@gmail.com>
2026-08-08 17:01:24 +0300

    clerk auth provider added with components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+
 function App() {
   return (
     <>
-      <h1>Hello universe</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode="modal" />
+          <SignUpButton mode="modal" />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
     </>
   );
 }
