62ab2bce3c980c3bc5f409f642a79ce9821b21aa
Ilay Zeitlin <zeitlinilya@gmail.com>
2026-08-11 19:08:37 +0300

    protected route fixed

--- server/src/middleware/protect-route.js
+++ server/src/middleware/protect-route.js
@@ -1,25 +1,8 @@
+import { getAuth } from "@clerk/express";
+
 function protectRoute(req, res, next) {
   try {
     const auth = getAuth(req);
-
-    console.log("AUTH:", auth);
-
-    console.log("USER ID:", auth.userId);
-    console.log("AUTH:", auth);
-
-    console.log("USER ID:", auth.userId);
-    console.log("AUTH:", auth);
-
-    console.log("USER ID:", auth.userId);
-    console.log("AUTH:", auth);
-
-    console.log("USER ID:", auth.userId);
-    console.log("AUTH:", auth);
-
-    console.log("USER ID:", auth.userId);
-    console.log("AUTH:", auth);
-
-    console.log("USER ID:", auth.userId);
 
     if (!auth.userId) {
       return res.status(401).send("User not authenticated");
